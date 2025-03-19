d8bf56eedaac1768c68965f0e14a69a98ddafa2d
Peyin0101 <randi.fernandez-moran@hotmail.com>
2025-03-19 13:12:03 +0100

    Implement authentication logic with validation for login and registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -11,15 +14,20 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function handleLogin()
+    public function handleLogin(Request $request)
     {
         // Valideer het formulier
         // Elk veld is verplicht
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'Required|min:8|confirmed',
+        ]);
 
         // Schrijf de aanmeld logica om in te loggen.
-
-        // Als je ingelogd bent stuur je de bezoeker door naar de intented "profile" route (zie hieronder)
-        return redirect()->intended(route('profile'));
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            // Als je ingelogd bent stuur je de bezoeker door naar de intented "profile" route (zie hieronder)
+            return redirect()->intended(route('profile'));
+        }
 
         // Als je gegevens fout zijn stuur je terug naar het formulier met
         // een melding voor het email veld dat de gegevens niet correct zijn.
@@ -31,12 +39,24 @@ class AuthController extends Controller
         return view('auth.register');
     }
 
-    public function handleRegister()
+    public function handleRegister(Request $request)
     {
         // Valideer het formulier.
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
         // Elk veld is verplicht / Wachtwoord en confirmatie moeten overeen komen / Email adres moet uniek zijn
         // Bewaar een nieuwe gebruiker in de databank met een beveiligd wachtwoord.
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
+        Auth::login($user);
         // BONUS: Verstuur een email naar de gebruiker waarin staat dat er een nieuwe account geregistreerd is voor de gebruiker.
 
         return redirect()->route('login');
